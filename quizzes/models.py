91ceb14cd736c75a7df879ffc2d84bb503bf45ee
iadokonat <iadokonat0106@gmail.com>
2023-01-02 19:30:26 +0900

    fix Quiz model

--- quizzes/models.py
+++ quizzes/models.py
@@ -9,6 +9,21 @@ class Quiz(models.Model):
     option3 = models.CharField("選択肢3", max_length=150)
     option4 = models.CharField("選択肢4", max_length=150)
     option5 = models.CharField("選択肢5", max_length=150)
+    answer1 = models.IntegerField(
+        "解答番号1", choices=((1, 1), (2, 2), (3, 3), (4, 4), (5, 5)), default=1
+    )
+    answer2 = models.IntegerField(
+        "解答番号2", choices=((1, 1), (2, 2), (3, 3), (4, 4), (5, 5)), null=True, blank=True
+    )
+    answer3 = models.IntegerField(
+        "解答番号3", choices=((1, 1), (2, 2), (3, 3), (4, 4), (5, 5)), null=True, blank=True
+    )
+    answer4 = models.IntegerField(
+        "解答番号4", choices=((1, 1), (2, 2), (3, 3), (4, 4), (5, 5)), null=True, blank=True
+    )
+    answer5 = models.IntegerField(
+        "解答番号5", choices=((1, 1), (2, 2), (3, 3), (4, 4), (5, 5)), null=True, blank=True
+    )
     created_by = models.ForeignKey(
         settings.AUTH_USER_MODEL, verbose_name="作成者", on_delete=models.CASCADE
     )
--- quizzes/views.py
+++ quizzes/views.py
@@ -1,26 +1,32 @@
+from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
 from django.views.generic import View
+
+from .models import Quiz
 
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        return TemplateResponse(request, "quizzes/index.html", {})
+        quizzes = Quiz.objects.select_related("created_by").all()
+        return TemplateResponse(request, "quizzes/index.html", {"quizzes": quizzes})
 
 
 index = IndexView.as_view()
 
 
 class ThinkingView(View):
-    def get(self, request, *args, **kwargs):
-        return TemplateResponse(request, "quizzes/thinking.html", {})
+    def get(self, request, quiz_id, *args, **kwargs):
+        quiz = get_object_or_404(Quiz, pk=quiz_id)
+        return TemplateResponse(request, "quizzes/thinking.html", {"quiz": quiz})
 
 
 thinking = ThinkingView.as_view()
 
 
 class ResultView(View):
-    def get(self, request, *args, **kwargs):
-        return TemplateResponse(request, "quizzes/result.html", {})
+    def get(self, request, quiz_id, *args, **kwargs):
+        quiz = get_object_or_404(Quiz, pk=quiz_id)
+        return TemplateResponse(request, "quizzes/result.html", {"quiz": quiz})
 
 
 result = ResultView.as_view()
